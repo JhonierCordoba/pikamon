e193315761cc5eaf18fbc5215068c4cd1ac02295
rainymarcelo <marcelo.delahozs@udea.edu.co>
2021-04-19 22:16:46 -0500

    Integracio cliente

---
+++ cliente.py
@@ -0,0 +1,11 @@
+import requests, json
+
+
+def getplayers(ip):
+    url = "http://" + ip + ":5000/players"
+
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.request("GET", url, headers=headers)
+    answerjson = response.json()
+    print(response.text)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json, requests
 from pikamon import *
 from stats import *
 import time
+from cliente import *
 
 # Creación de los pikamons
 pikamon1 = Pikamon("Marciu", 100, "Grass")
@@ -10,17 +12,15 @@ pikamon4 = Pikamon("añañañin", 100, "ñatus")
 pikamon5 = Pikamon("Goku", 7, "Jesuscrist")
 pikamon6 = Pikamon("oni-chan", 69, "hentai")
 
-
-
 # Stats de pikamons
 
-#Bulbasur
+# Bulbasur
 pikamon1.currentHp = 45
 pikamon1.defense = 49
 pikamon1.speed = 45
 pikamon1.spadefense = 65
 pikamon1.spattack = 65
-#Chimchar
+# Chimchar
 pikamon2.currentHp = 44
 pikamon2.defense = 44
 pikamon2.speed = 58
@@ -45,16 +45,27 @@ pikamon1.currentHp = 45
 pikamon1.defense = 49
 pikamon1.speed = 45"""
 
-pikamon1.attacks = [Attack("Tackle", "Physical", 35, 100), Attack("Vine wip", "Grass", 45, 100), Attack("Sweet Scent", "Effect", 0, 100)]
-pikamon2.attacks = [Attack("Scratch", "Physical", 40, 100), Attack("Fire Spin", "Fire", 50, 90), Attack("Flame Wheel", "Fire", 60, 85)]
-pikamon3.attacks = [Attack("Tackle", "Physical", 40, 100), Attack("Water Gun", "Water", 40, 100), Attack("Hydro Pump", "Water", 110, 80)]
+pikamon1.attacks = [Attack("Tackle", "Physical", 35, 100), Attack("Vine wip", "Grass", 45, 100),
+                    Attack("Sweet Scent", "Effect", 0, 100)]
+pikamon2.attacks = [Attack("Scratch", "Physical", 40, 100), Attack("Fire Spin", "Fire", 50, 90),
+                    Attack("Flame Wheel", "Fire", 60, 85)]
+pikamon3.attacks = [Attack("Tackle", "Physical", 40, 100), Attack("Water Gun", "Water", 40, 100),
+                    Attack("Hydro Pump", "Water", 110, 80)]
 pikamon4.attacks = [Attack("scratch", "plant", 100, 93)]
 pikamon5.attacks = [Attack("scratch", "plant", 100, 93)]
 pikamon6.attacks = [Attack("scratch", "plant", 100, 93)]
 
 # A pelear!
-battle = Battle(pikamon1, pikamon2)
 
+import requests
+
+ip = input("Ingrese la ip del servidor:")
+player_name = input("ingrese su nick: ")
+player_pikamon = input("ingrese el nombre del pikamon a usar: ")
+
+getplayers(ip)
+
+battle = Battle(pikamon1, pikamon2)
 
 def tuTurno(pikamon):
     comando = None
